d94412f91b4783b1efc43802d1b8a9dd91dea607
mohamedkamalpharm <108032406+statpharm@users.noreply.github.com>
2025-01-09 23:40:32 +0200

    Add files via upload

---
+++ doc/SampleSizeSingleArmSurvival.R
@@ -0,0 +1,29 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- results = "hide", message = FALSE, warning = FALSE----------------------
+# From source
+devtools::install_github("statpharm/SampleSizeSingleArmSurvival")
+
+
+## -----------------------------------------------------------------------------
+library(SampleSizeSingleArmSurvival)
+required_sample <- calcSampleSizeArcsine(S0 = 0.90, S1 = 0.96)
+required_sample
+
+## -----------------------------------------------------------------------------
+custom_sample <- calcSampleSizeArcsine(
+  S0 = 0.80,
+  S1 = 0.85,
+  accrual = 36,
+  followup = 24,
+  timePoint = 18
+)
+custom_sample
+
+## -----------------------------------------------------------------------------
+result <- calcSampleSizeArcsine(S0 = 0.90, S1 = 0.90)
+
